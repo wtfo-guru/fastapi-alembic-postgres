db8784849b8a25581fc4723901475c22737974f2
Quien Sabe <qs5779@mail.com>
2023-05-17 08:53:24 -0400

    Poetry update ... (#10)
    * docs: Update todo
    
    * fix: Remove passlib requirement, use bcrypt instead
    
    * chore: Move to laptop
    
    * chore: LtM

--- TODO.md
+++ TODO.md
@@ -1,3 +1,7 @@
-# Stuff that needs to be done
+# TODO: Items
 
-1. find or create a replacement for the passlib library which appears un-maintained
+1. Test/verify login code
+
+# DONE: Items
+
+1. find or create a replacement for the passlib library which appears un-maintained [alternatives](https://pythfastapi-alembic-sqlalchemyon.libhunt.com/passlib-alternatives) just used bcrypt see app/core/hashing.py
--- app/core/hashing.py
+++ app/core/hashing.py
@@ -1,13 +1,33 @@
-from passlib.context import CryptContext
+import bcrypt
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+KUTF8 = "utf-8"
 
 
 class Hasher:
     @classmethod
     def verify_password(cls, plain_password: str, hashed_password: str) -> bool:
-        return pwd_context.verify(plain_password, hashed_password)
+        """Verify a plain password.
+
+        Args:
+            plain_password (str): clear text password
+            hashed_password (str): hashed password
+
+        Returns:
+            bool: True if the password matches the hashed password
+        """
+        return bcrypt.checkpw(
+            plain_password.encode(KUTF8),
+            hashed_password.encode(KUTF8),
+        )
 
     @classmethod
     def get_password_hash(cls, password: str) -> str:
-        return pwd_context.hash(password)
+        """Create a hash of the password.
+
+        Args:
+            password (str): clear text password
+
+        Returns:
+            str: hashed password
+        """
+        return bcrypt.hashpw(password.encode(KUTF8), bcrypt.gensalt()).decode(KUTF8)
